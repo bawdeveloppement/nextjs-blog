94cd3c3cdfd99b9f0d0ebf50b27f188b099f3338
bawdeveloppement <baw.developpement@gmail.com>
2020-09-20 05:50:18 +0200

    cache route

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
+  const [isOnline, setIsOnline] = useState(true);
+  
+  useEffect(() => {
+    if (typeof window !== 'undefined' && 'ononline' in window && 'onoffline' in window) {
+      setIsOnline(window.navigator.onLine)
+      if (!window.ononline) {
+        window.addEventListener('online', () => {
+          setIsOnline(true)
+        })
+      }
+      if (!window.onoffline) {
+        window.addEventListener('offline', () => {
+          setIsOnline(false)
+        })
+      }
+    }
+  }, [])
+
+  const router = useRouter();
+  
+  useEffect(() => {
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator && window.workbox !== undefined && isOnline) {
+      // skip index route, because it's already cached under `start-url` caching object
+      if (router.route !== '/') {
+        const wb = window.workbox
+        wb.active.then(worker => {
+          wb.messageSW({ action: 'CACHE_NEW_ROUTE' })
+        })
+      }
+    }
+  }, [isOnline, router.route])
+
   return <> 
     <Head>
       <meta charSet="utf-8" />
